2eb1fd465800133d193206304156cc29b0548a41
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-05-29 15:41:20 +0200

    Add files via upload

---
+++ textEditor/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace EditorTeksta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtbIspis = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsmNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsbBold = new System.Windows.Forms.ToolStripButton();
+            this.tsbItalic = new System.Windows.Forms.ToolStripButton();
+            this.tsbUnderLine = new System.Windows.Forms.ToolStripButton();
+            this.tsbPlain = new System.Windows.Forms.ToolStripButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbIspis
+            // 
+            this.rtbIspis.Location = new System.Drawing.Point(12, 33);
+            this.rtbIspis.Margin = new System.Windows.Forms.Padding(4);
+            this.rtbIspis.Name = "rtbIspis";
+            this.rtbIspis.Size = new System.Drawing.Size(1041, 508);
+            this.rtbIspis.TabIndex = 0;
+            this.rtbIspis.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.tsbBold,
+            this.tsbItalic,
+            this.tsbUnderLine,
+            this.tsbPlain});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1067, 27);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmNew,
+            this.tsmSave,
+            this.tsmSaveAs,
+            this.tsmOpen});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(34, 24);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // tsmNew
+            // 
+            this.tsmNew.Name = "tsmNew";
+            this.tsmNew.Size = new System.Drawing.Size(224, 26);
+            this.tsmNew.Text = "New";
+            this.tsmNew.Click += new System.EventHandler(this.tsmNew_Click);
+            // 
+            // tsmSave
+            // 
+            this.tsmSave.Name = "tsmSave";
+            this.tsmSave.Size = new System.Drawing.Size(224, 26);
+            this.tsmSave.Text = "Save";
+            this.tsmSave.Click += new System.EventHandler(this.tsmSave_Click);
+            // 
+            // tsmSaveAs
+            // 
+            this.tsmSaveAs.Name = "tsmSaveAs";
+            this.tsmSaveAs.Size = new System.Drawing.Size(224, 26);
+            this.tsmSaveAs.Text = "Save as";
+            this.tsmSaveAs.Click += new System.EventHandler(this.tsmSaveAs_Click);
+            // 
+            // tsmOpen
+            // 
+            this.tsmOpen.Name = "tsmOpen";
+            this.tsmOpen.Size = new System.Drawing.Size(224, 26);
+            this.tsmOpen.Text = "Open";
+            this.tsmOpen.Click += new System.EventHandler(this.tsmOpen_Click);
+            // 
+            // tsbBold
+            // 
+            this.tsbBold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbBold.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tsbBold.Image = ((System.Drawing.Image)(resources.GetObject("tsbBold.Image")));
+            this.tsbBold.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBold.Name = "tsbBold";
+            this.tsbBold.Size = new System.Drawing.Size(45, 24);
+            this.tsbBold.Text = "Bold";
+            this.tsbBold.Click += new System.EventHandler(this.tsbBold_Click);
+            // 
+            // tsbItalic
+            // 
+            this.tsbItalic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbItalic.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tsbItalic.Image = ((System.Drawing.Image)(resources.GetObject("tsbItalic.Image")));
+            this.tsbItalic.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbItalic.Name = "tsbItalic";
+            this.tsbItalic.Size = new System.Drawing.Size(45, 24);
+            this.tsbItalic.Text = "Italic";
+            this.tsbItalic.Click += new System.EventHandler(this.tsbItalic_Click_1);
+            // 
+            // tsbUnderLine
+            // 
+            this.tsbUnderLine.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbUnderLine.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tsbUnderLine.Image = ((System.Drawing.Image)(resources.GetObject("tsbUnderLine.Image")));
+            this.tsbUnderLine.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbUnderLine.Name = "tsbUnderLine";
+            this.tsbUnderLine.Size = new System.Drawing.Size(80, 24);
+            this.tsbUnderLine.Text = "UnderLine";
+            this.tsbUnderLine.Click += new System.EventHandler(this.tsbUnderLine_Click);
+            // 
+            // tsbPlain
+            // 
+            this.tsbPlain.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbPlain.Image = ((System.Drawing.Image)(resources.GetObject("tsbPlain.Image")));
+            this.tsbPlain.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPlain.Name = "tsbPlain";
+            this.tsbPlain.Size = new System.Drawing.Size(45, 24);
+            this.tsbPlain.Text = "Plain";
+            this.tsbPlain.Click += new System.EventHandler(this.tsbPlain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.rtbIspis);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbIspis;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem tsmNew;
+        private System.Windows.Forms.ToolStripMenuItem tsmSave;
+        private System.Windows.Forms.ToolStripMenuItem tsmSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem tsmOpen;
+        private System.Windows.Forms.ToolStripButton tsbBold;
+        private System.Windows.Forms.ToolStripButton tsbItalic;
+        private System.Windows.Forms.ToolStripButton tsbUnderLine;
+        private System.Windows.Forms.ToolStripButton tsbPlain;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ textEditor/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EditorTeksta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsbBold_Click(object sender, EventArgs e)
+        {
+            rtbIspis.Font = new Font(rtbIspis.Font, FontStyle.Bold);
+        }
+        private void tsbItalic_Click_1(object sender, EventArgs e)
+        {
+            rtbIspis.Font = new Font(rtbIspis.Font, FontStyle.Italic);
+        }
+
+        private void tsbUnderLine_Click(object sender, EventArgs e)
+        {
+            rtbIspis.Font = new Font(rtbIspis.Font, FontStyle.Underline);
+        }
+
+        private void tsbPlain_Click(object sender, EventArgs e)
+        {
+            rtbIspis.Font = new Font(rtbIspis.Font, FontStyle.Regular);
+        }
+
+        private void tsmNew_Click(object sender, EventArgs e)
+        {
+            rtbIspis.Clear();
+        }
+
+        private void tsmOpen_Click(object sender, EventArgs e)
+        {
+            Stream st;
+            OpenFileDialog d1=new OpenFileDialog();
+            if (d1.ShowDialog() == DialogResult.OK)
+            {
+                if ((st = d1.OpenFile()) != null)
+                {
+                    StreamReader stream = new StreamReader(d1.FileName);
+                    string datoteka = stream.ReadToEnd();
+                    rtbIspis.Rtf = datoteka.ToString();
+                    stream.Close();
+                }
+            }
+        }
+
+        private void tsmSave_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(@"c:\TextEditor"))
+            {
+                Directory.CreateDirectory(@"c:\TextEditor");
+                string path = @"c:\TextEditor\text.rtf";
+                if (File.Exists(path))
+                {
+                    StreamWriter sw = new StreamWriter(path);
+                    sw.WriteLine(rtbIspis.Rtf);
+                    sw.Close();
+                }
+                else
+                {
+                    File.Create(path);
+                }
+
+            }
+            else if (Directory.Exists(@"c:\TextEditor"))
+            {
+
+                string path = @"c:\TextEditor\text.rtf";
+                if (File.Exists(path))
+                {
+                    StreamWriter sw = new StreamWriter(path);
+                    sw.WriteLine(rtbIspis.Rtf);
+                    sw.Close();
+                }
+                else
+                {
+                    File.Create(path);
+                }
+            }
+        }
+
+        private void tsmSaveAs_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog dr = new SaveFileDialog();
+            dr.Filter = "Doc File|*.rtf";
+            if (dr.ShowDialog() == DialogResult.OK)
+            {
+                rtbIspis.SaveFile(dr.FileName);
+                MessageBox.Show("Saved successfully!", "Address File : "
+                    + dr.FileName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+    }
+}
